88b4d09c3f8ac9acf50201e42377b708efb469e1
Samuel Mabuka <samuelmabuka@gmail.com>
2025-07-11 21:53:05 +0300

    directory

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,3 +1,4 @@
+-- 1. INNER JOIN: Bookings and their Users
 SELECT 
   bookings.booking_id,
   bookings.property_id,
@@ -7,6 +8,8 @@ SELECT
 FROM bookings
 INNER JOIN users ON bookings.user_id = users.user_id;
 
+
+-- 2. LEFT JOIN: Properties and their Reviews (include properties without reviews)
 SELECT
   properties.title,
   reviews.rating,
@@ -15,6 +18,7 @@ FROM properties
 LEFT JOIN reviews ON properties.property_id = reviews.property_id;
 
 
+-- 3. FULL OUTER JOIN: All Users and All Bookings (even unmatched)
 SELECT 
   users.user_id,
   users.name,
@@ -23,3 +27,31 @@ SELECT
   bookings.booking_date
 FROM users
 FULL OUTER JOIN bookings ON users.user_id = bookings.user_id;
+
+
+-- 4. ORDER BY: Bookings sorted by most recent
+SELECT 
+  bookings.booking_id,
+  bookings.booking_date,
+  users.name
+FROM bookings
+INNER JOIN users ON bookings.user_id = users.user_id
+ORDER BY bookings.booking_date DESC;
+
+
+-- 5. ORDER BY: Properties listed alphabetically
+SELECT
+  properties.title,
+  reviews.rating
+FROM properties
+LEFT JOIN reviews ON properties.property_id = reviews.property_id
+ORDER BY properties.title ASC;
+
+
+-- 6. ORDER BY: Sort by user name and booking date
+SELECT 
+  users.name,
+  bookings.booking_date
+FROM users
+FULL OUTER JOIN bookings ON users.user_id = bookings.user_id
+ORDER BY users.name ASC, bookings.booking_date DESC;
