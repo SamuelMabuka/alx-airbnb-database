55d053d73b6578515494916a0c7174c4f5df0005
Samuel Mabuka <samuelmabuka@gmail.com>
2025-07-11 21:35:52 +0300

    directory

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,26 @@
+SELECT 
+  bookings.booking_id,
+  bookings.property_id,
+  bookings.booking_date,
+  users.user_id,
+  users.name
+FROM bookings
+INNER JOIN users ON bookings.user_id = users.user_id;
+
+SELECT 
+  properties.property_id,
+  properties.title,
+  reviews.review_id,
+  reviews.rating,
+  reviews.comment
+FROM properties
+LEFT JOIN reviews ON properties.property_id = reviews.property_id;
+
+SELECT 
+  users.user_id,
+  users.name,
+  bookings.booking_id,
+  bookings.property_id,
+  bookings.booking_date
+FROM users
+FULL OUTER JOIN bookings ON users.user_id = bookings.user_id;
