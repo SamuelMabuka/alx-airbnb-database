0a25a78442dcc5d43c49fbc4a1442bf2028f5d20
Samuel Mabuka <samuelmabuka@gmail.com>
2025-07-11 22:08:52 +0300

    aggregations

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,3 +1,4 @@
+-- Step 1: Create a summary of total bookings per property
 WITH property_bookings AS (
   SELECT 
     property_id,
@@ -5,8 +6,18 @@ WITH property_bookings AS (
   FROM bookings
   GROUP BY property_id
 )
+
+-- Step 2a: Use ROW_NUMBER to assign unique ranks (no ties)
 SELECT 
   property_id,
   total_bookings,
   ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS row_number
+FROM property_bookings;
+
+
+-- Step 2b: Use RANK to allow ties in ranking
+SELECT 
+  property_id,
+  total_bookings,
+  RANK() OVER (ORDER BY total_bookings DESC) AS rank
 FROM property_bookings;
